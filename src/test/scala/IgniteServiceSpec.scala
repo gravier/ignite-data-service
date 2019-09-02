6d6f3359a05941b4a26ab63e70a3fae0dd77ac08
evaldas <e.miliauskas@fuzzylabsresearch.com>
2019-09-02 20:38:34 +0300

    test case for reader from csv

--- src/test/scala/IgniteServiceSpec.scala
+++ src/test/scala/IgniteServiceSpec.scala
@@ -20,6 +20,7 @@ import org.scalatest.concurrent.ScalaFutures
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.io.Source
+import scala.util.Try
 
 class IgniteServiceSpec extends FlatSpec with Matchers with ScalatestRouteTest with PropertyService {
   override def testConfigSource     = "akka.loglevel = WARNING"
@@ -39,7 +40,6 @@ class IgniteServiceSpec extends FlatSpec with Matchers with ScalatestRouteTest w
              46.222,
              3000000,
              "brl",
-             3000000,
              773724,
              200,
              250,
@@ -68,42 +68,93 @@ class IgniteServiceSpec extends FlatSpec with Matchers with ScalatestRouteTest w
         ch
       }
     }
-
+//  implicit val cache: PropertyIgniteCache                          = null
   implicit val sqlCtx: SqlMirrorContext[MirrorSqlDialect, Literal] = new SqlMirrorContext(MirrorSqlDialect, Literal)
   implicit val propertyRepository                                  = new PropertyRepository()
 
-//  it should "return all results" in {
-//    Post(s"/property", FindByLocation()) ~> routes ~> check {
-//      status shouldBe OK
-//      contentType shouldBe `application/json`
-//      val resp = responseAs[List[Property]]
-//      resp shouldBe properties
-//    }
-//  }
-//
-//  it should "filter by state" in {
-//    Post(s"/property", FindByLocation(state = Some("rio"))) ~> routes ~> check {
-//      status shouldBe OK
-//      contentType shouldBe `application/json`
-//      val resp = responseAs[List[Property]]
-//      resp shouldBe properties.filter(_.state == "rio")
-//    }
-//
-//    Post(s"/property", FindByLocation(state = Some("rio1"))) ~> routes ~> check {
-//      status shouldBe OK
-//      contentType shouldBe `application/json`
-//      val resp = responseAs[List[Property]]
-//      resp shouldBe List()
-//    }
-//  }
+  it should "return all results" in {
+    Post(s"/property", FindByLocation()) ~> routes ~> check {
+      status shouldBe OK
+      contentType shouldBe `application/json`
+      val resp = responseAs[List[Property]]
+      resp shouldBe properties
+    }
+  }
+
+  it should "filter by state" in {
+    Post(s"/property", FindByLocation(state = Some("rio"))) ~> routes ~> check {
+      status shouldBe OK
+      contentType shouldBe `application/json`
+      val resp = responseAs[List[Property]]
+      resp shouldBe properties.filter(_.state == "rio")
+    }
+
+    Post(s"/property", FindByLocation(state = Some("rio1"))) ~> routes ~> check {
+      status shouldBe OK
+      contentType shouldBe `application/json`
+      val resp = responseAs[List[Property]]
+      resp shouldBe List()
+    }
+  }
 
   it should "be possible to load data" in {
     import kantan.csv._
     import kantan.csv.ops._
     import kantan.csv.generic._
 
-    val url      = new URL("https://storage.googleapis.com/stacktome-temp/property-br-sample.csv")
-    val iterator = url.asCsvReader[Property](rfc.withHeader)
+//    val url      = new URL("https://storage.googleapis.com/stacktome-temp/property-br-sample.csv")
+    val url = new URL("file:////home/evaldas/Downloads/property-br-sample.csv")
+
+    val iterator = url.asCsvReader[List[String]](rfc.withHeader)
+    val res = for { row <- iterator } yield
+      row.getOrElse(List()) match {
+        case (id ::
+              createdOn ::
+              propertyType ::
+              place ::
+              state ::
+              lat ::
+              lon ::
+              price ::
+              currency ::
+              priceUsd ::
+              surficeTotalSqm ::
+              surficeCoveredSqm ::
+              priceUsdPerSqm ::
+              pricePerSqm ::
+              floor ::
+              rooms ::
+              expenses ::
+              url ::
+              title ::
+              imageUrl :: _) =>
+          Some(
+            Property(
+              id,
+              Try(org.joda.time.LocalDate.parse(createdOn).toDate.toInstant.toEpochMilli).getOrElse(0),
+              propertyType,
+              place,
+              state,
+              Try(lat.toDouble).getOrElse(0.0),
+              Try(lon.toDouble).getOrElse(0.0),
+              Try(price.toDouble).getOrElse(0.0),
+              currency,
+              Try(priceUsd.toDouble).getOrElse(0.0),
+              Try(surficeTotalSqm.toInt).getOrElse(0),
+              Try(surficeCoveredSqm.toInt).getOrElse(0),
+              Try(priceUsdPerSqm.toDouble).getOrElse(0.0),
+              Try(pricePerSqm.toDouble).getOrElse(0.0),
+              Try(floor.toInt).getOrElse(0),
+              Try(rooms.toInt).getOrElse(0),
+              Try(expenses.toInt).getOrElse(0),
+              url,
+              title,
+              imageUrl
+            ))
+        case _ =>
+          None
+
+      }
   }
 
 }
